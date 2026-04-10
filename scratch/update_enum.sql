f4b6bdf8fe03619da1e291c8cd411eb548011610
ashaheer-svg <ashaheer@gmail.com>
2026-04-10 09:52:43 +0530

    1

--- scratch/update_enum.sql
+++ scratch/update_enum.sql
@@ -1,2 +1,2 @@
-ALTER TYPE scan_job_status ADD VALUE IF NOT EXISTS 'error';
-ALTER TYPE scan_job_status ADD VALUE IF NOT EXISTS 'retry';
+ALTER TYPE scan_status ADD VALUE IF NOT EXISTS 'error';
+ALTER TYPE scan_status ADD VALUE IF NOT EXISTS 'retry';
