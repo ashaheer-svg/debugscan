98f7bf399cf1c6c2ee7e4a4bc22a71175a33224b
ashaheer-svg <ashaheer@gmail.com>
2026-04-17 16:56:06 +0530

    1

--- src/Controllers/AdminController.php
+++ src/Controllers/AdminController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use App\Services\AiService;
+use App\Services\MailService;
 use PDO;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
