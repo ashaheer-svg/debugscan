df9fc303a2d1dea33f8edcf83044182845c6306e
ashaheer-svg <ashaheer@gmail.com>
2026-04-19 10:06:54 +0530

    Fix token deduction visibility mismatch

--- src/Controllers/TenantController.php
+++ src/Controllers/TenantController.php
@@ -953,7 +953,7 @@ class TenantController
             $details = json_decode($log['details'], true) ?: [];
             
             // Extract tokens change
-            $tokenChange = $details['amount'] ?? ($details['used'] ?? '0');
+            $tokenChange = $details['amount_deducted'] ?? ($details['amount'] ?? ($details['used'] ?? '0'));
             if (in_array($log['action'], ['tokens_deducted', 'scan_completed'])) {
                 $tokenChange = '-' . $tokenChange;
             } elseif (in_array($log['action'], ['tokens_allocated', 'tokens_redeemed'])) {
